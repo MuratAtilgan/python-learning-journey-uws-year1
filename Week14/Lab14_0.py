46df3f931c8d5548f5382b4644e5152de91bd85c
MuratAtilgan <muratatilgan@hotmail.co.uk>
2026-02-09 23:32:24 +0000

    Add files via upload

---
+++ Week14/Lab14_0.py
@@ -0,0 +1,184 @@
+class Team:
+    def __init__(self,name,colour):
+        self.name=name
+        self.colour=colour
+    
+    def get_name(self):
+        return self.name
+    
+    def get_colour(self):
+        return self.colour
+    
+class Football(Team):
+
+    sport="Football"
+
+    def __init__(self, name, colour,star_player):
+        super().__init__(name, colour)
+        self.star_player=star_player
+
+    def display_team_info(self):
+        print(f"Team:\t\t{self.name}")
+        print(f"Sport:\t\t{self.sport}")
+        print(f"Colour:\t\t{self.colour}")
+        print(f"Star Player:\t{self.star_player}")
+
+class Rugby(Team):
+
+    sport="Rugby"
+
+    def __init__(self, name, colour,flyhalf):
+        super().__init__(name, colour)
+        
+        self.flyhalf=flyhalf
+
+    def display_team_info(self):
+        print(f"Team:\t\t{self.name}")
+        print(f"Sport:\t\t{self.sport}")
+        print(f"Colour:\t\t{self.colour}")
+        print(f"Flyhalf:\t{self.flyhalf}")
+
+
+class Ice_Hockey(Team):
+
+    sport="Ice_Hockey"
+
+    def __init__(self, name, colour,goalie):
+        super().__init__(name, colour)
+
+        self.goalie=goalie
+
+    def display_team_info(self):
+        print(f"Team:\t\t{self.name}")
+        print(f"Sport:\t\t{self.sport}")
+        print(f"Colour:\t\t{self.colour}")
+        print(f"Goalie:\t\t{self.goalie}")
+
+
+class Shinty(Team):
+
+    sport="Shinty"
+
+    def __init__(self, name, colour,captain):
+        super().__init__(name, colour)
+
+        self.captain=captain
+
+    def display_team_info(self):
+        print(f"Team:\t\t{self.name}")
+        print(f"Sport:\t\t{self.sport}")
+        print(f"Colour:\t\t{self.colour}")
+        print(f"Captain:\t{self.captain}")
+
+
+##MENU
+
+def select():
+    print()
+    print("="*75)
+    print("<:::> TEAM DATABASE <:::>")
+    print("="*75)
+    print("1 ..... FOOTBALL")
+    print("2 ..... RUGBY")
+    print("3 ..... ICE HOCKEY")
+    print("4 ..... SHINTY")
+    print("5 ..... VIEW ALL TEAMS")
+    print("6 ..... QUIT PROGRAM")
+    print("="*75)
+    option = input("Select: ")
+    print()
+    return option
+
+def quit_program():
+    print("="*75)
+    print("<:::> GOOD BYE <:::>")
+    print("="*75)
+
+def invalid_entry():
+    print("Invalid ENTRY!!! Please select 1,2,3,4,5 and 6")
+
+
+teams=[]
+
+selection=select()
+
+
+##MAIN PROGRAM
+
+while True:
+    
+    if selection == "1":
+        name=input("Enter the Team:\t\t")
+        colour=input("Enter the colours:\t")
+        star_player=input("Who is the star player:\t")    
+        teams.append(Football(name,colour,star_player))   
+        print()
+        print("="*50)
+        print(f"<::> {teams[-1].__class__.__name__} TEAM ADDED SUCCESSFULLY <::>")  ##confirmation message
+        print()
+        teams[-1].display_team_info()   ##Calls method to show all details
+        print("="*50)
+
+    elif selection == "2":
+        name=input("Enter the Team:\t\t")
+        colour=input("Enter the colours:\t")
+        flyhalf=input("Who is the flyhalf:\t")    
+        teams.append(Rugby(name,colour,flyhalf))   
+        print()
+        print("="*50)
+        print(f"<::> {teams[-1].__class__.__name__} TEAM ADDED SUCCESSFULLY <::>") 
+        print() 
+        teams[-1].display_team_info()   
+        print("="*50)
+
+    elif selection == "3":
+        name=input("Enter the Team:\t\t")
+        colour=input("Enter the colours:\t")
+        goalie=input("Who is the goalie:\t")    
+        teams.append(Ice_Hockey(name,colour,goalie))   
+        print()
+        print("="*50)
+        print(f"<::> {teams[-1].__class__.__name__} TEAM ADDED SUCCESSFULLY <::>") 
+        print() 
+        teams[-1].display_team_info()   
+        print("="*50)
+
+    
+    elif selection == "4":
+        name=input("Enter the Team:\t\t")
+        colour=input("Enter the colours:\t")
+        captain=input("Who is the captain:\t")    
+        teams.append(Shinty(name,colour,captain))   
+        print()
+        print("="*50)
+        print(f"<::> {teams[-1].__class__.__name__} TEAM ADDED SUCCESSFULLY <::>")  
+        print()
+        teams[-1].display_team_info()   
+        print("="*50)
+
+    elif selection == "5":
+        print("="*75)
+        print("ALL TEAMS")
+        print("="*75)
+        for team in teams:
+            team.display_team_info()
+        print("="*75)
+
+    
+    elif selection == "6":
+        quit_program()
+        break
+
+    else:
+        invalid_entry()
+
+    selection=select()
+
+
+
+
+
+
+    
+
+    
